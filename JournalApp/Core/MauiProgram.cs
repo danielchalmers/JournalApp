4fd4155c88804e7948508f6b690fd5897e05f422
Daniel Chalmers <daniel.chalmers@outlook.com>
2022-03-26 18:58:05 -0500

    Prep

--- JournalApp/Core/MauiProgram.cs
+++ JournalApp/Core/MauiProgram.cs
@@ -6,17 +6,13 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        var builder = MauiApp.CreateBuilder();
-
-        builder
+        var builder = MauiApp.CreateBuilder()
             .RegisterBlazorMauiWebView()
             .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            });
+            .ConfigureFonts(fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"));
 
         builder.Services.AddBlazorWebView();
+        builder.Services.AddAntDesign();
 
         return builder.Build();
     }
