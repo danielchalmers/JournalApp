1f49be7c4086e125874254be7eb327e8128518d6
Daniel Chalmers <daniel.chalmers@outlook.com>
2022-05-23 19:17:06 -0500

    Microsoft.Maui.Templates::6.0.312 RTM

--- JournalApp/Core/MauiProgram.cs
+++ JournalApp/Core/MauiProgram.cs
@@ -11,6 +11,9 @@ public static class MauiProgram
             .ConfigureFonts(fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"));
 
         builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
         builder.Services.AddAntDesign();
 
         builder.Services.AddDbContextFactory<ApplicationDbContext>(options => options
