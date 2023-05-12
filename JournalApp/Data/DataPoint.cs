6476e959b4cd94327735f569d9d300bac302ac37
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-05-12 18:08:02 -0500

    Delete empty notes

--- JournalApp/Data/DataPoint.cs
+++ JournalApp/Data/DataPoint.cs
@@ -29,6 +29,8 @@ public class DataPoint
 
     public virtual DataPointCategory Category { get; set; }
 
+    public bool IsDeleted { get; set; }
+
     public DateTimeOffset CreatedAt { get; set; }
 
     public DataType DataType { get; set; }
