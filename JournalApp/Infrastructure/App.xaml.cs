78d52bdac2f23f63616ec86a9240e4d45ba2c002
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-05-20 20:34:51 -0500

    Handle main window deactivation from index
    For some reason injecting the db context in two different places makes the change tracking not work right. No idea why so I'm just moving it to the index for now.
    
    Also changes some controls to be Immediate so closing the window while they're in focus won't lose the changes.

--- JournalApp/Infrastructure/App.xaml.cs
+++ JournalApp/Infrastructure/App.xaml.cs
@@ -1,34 +1,11 @@
-﻿using System.Diagnostics;
-
-namespace JournalApp;
+﻿namespace JournalApp;
 
 public partial class App : Application
 {
-    private readonly ApplicationDbContext _dbContext;
-
-    public App(ApplicationDbContext dbContext)
+    public App()
     {
         InitializeComponent();
-        _dbContext = dbContext;
 
         MainPage = new MainPage();
-    }
-
-    protected override Window CreateWindow(IActivationState activationState)
-    {
-        var window = base.CreateWindow(activationState);
-        if (window != null)
-        {
-            window.Deactivated += Window_Deactivated;
-            window.Destroying += Window_Deactivated;
-        }
-
-        return window;
-    }
-
-    private async void Window_Deactivated(object sender, EventArgs e)
-    {
-        Debug.WriteLine("Window was deactivated or is being destroyed; Saving database...");
-        await _dbContext.SaveChangesAsync();
     }
 }
