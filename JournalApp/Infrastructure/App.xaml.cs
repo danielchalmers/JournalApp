fcfc595079a26759026126048b2eeb90011849c2
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-05-20 20:19:33 -0500

    Save DB every time the app loses focus or closes
    Otherwise we'd lose all unsaved data.

--- JournalApp/Infrastructure/App.xaml.cs
+++ JournalApp/Infrastructure/App.xaml.cs
@@ -1,11 +1,34 @@
-﻿namespace JournalApp;
+﻿using System.Diagnostics;
+
+namespace JournalApp;
 
 public partial class App : Application
 {
-    public App()
+    private readonly ApplicationDbContext _dbContext;
+
+    public App(ApplicationDbContext dbContext)
     {
         InitializeComponent();
+        _dbContext = dbContext;
 
         MainPage = new MainPage();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        if (window != null)
+        {
+            window.Deactivated += Window_Deactivated;
+            window.Destroying += Window_Deactivated;
+        }
+
+        return window;
+    }
+
+    private async void Window_Deactivated(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Window was deactivated or is being destroyed; Saving database...");
+        await _dbContext.SaveChangesAsync();
     }
 }
