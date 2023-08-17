339fe8b3e1fa8db3f0c0778c9315f7b16dc2d7d7
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-08-16 21:09:24 -0500

    Refactor mood grid
    Moves a lot of the calculations to the code behind instead of the markup.

--- JournalApp/Util/DateUtil.cs
+++ JournalApp/Util/DateUtil.cs
@@ -2,7 +2,13 @@
 
 public static class DateUtil
 {
-    public static DateOnly GetNextDate(this DateOnly dateOnly) => DateOnly.FromDayNumber(dateOnly.DayNumber + 1);
+	public static IEnumerable<DateOnly> GetDatesTo(this DateOnly startDate, DateOnly endDate)
+	{
+		for (var date = startDate; date <= endDate; date = date.AddDays(1))
+			yield return date;
+	}
 
-    public static DateOnly GetPreviousDate(this DateOnly dateOnly) => DateOnly.FromDayNumber(dateOnly.DayNumber - 1);
+	public static DateOnly GetNextDate(this DateOnly dateOnly) => DateOnly.FromDayNumber(dateOnly.DayNumber + 1);
+
+	public static DateOnly GetPreviousDate(this DateOnly dateOnly) => DateOnly.FromDayNumber(dateOnly.DayNumber - 1);
 }
