016fab65183b1c0e2e05209230ca47860972308b
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-08-20 12:39:32 -0500

    Optimize mood grid further by pre-filtering lists
    Takes the average creation time of a half full GridYear from 350ms to 50ms on my S23. May be even faster in Release

--- JournalApp/Components/MoodGrid/MoodGridUnits.cs
+++ JournalApp/Components/MoodGrid/MoodGridUnits.cs
@@ -8,24 +8,22 @@ public readonly struct GridMonth
 {
     private readonly DateTimeFormatInfo _dateTimeFormat = CultureInfo.CurrentCulture.DateTimeFormat;
 
-    public GridMonth(int month, HashSet<DataPoint> dataPoints, IEnumerable<DateOnly> dates)
+    public GridMonth(int month, IEnumerable<DataPoint> points, IEnumerable<DateOnly> dates)
     {
         Month = month;
         Name = _dateTimeFormat.GetMonthName(Month);
-        DataPoints = dataPoints;
         Dates = dates.ToList();
-        GridDays = GetGridDays().ToList();
+        GridDays = GetGridDays(points.ToHashSet()).ToList();
         DaysOfWeek = GetDaysOfWeek().ToList();
     }
 
     public int Month { get; }
     public string Name { get; }
-    public HashSet<DataPoint> DataPoints { get; }
     public IReadOnlyList<DateOnly> Dates { get; }
     public IReadOnlyList<GridDay> GridDays { get; }
     public IReadOnlyList<DayOfWeek> DaysOfWeek { get; }
 
-    private string GetMoodColor(string emoji) => emoji switch
+    private static string GetMoodColor(string emoji) => emoji switch
     {
         // https://www.colorhexa.com/0091ea-to-ff6d00
         "😄" => "#0091ea",
@@ -38,30 +36,29 @@ public readonly struct GridMonth
         _ => "transparent"
     };
 
-    private record DataPointLookup(DataPoint Point, DateOnly Date);
+    private sealed record DataPointLookup(DataPoint Point, DateOnly Date);
 
-    private IEnumerable<GridDay> GetGridDays()
+    private IEnumerable<GridDay> GetGridDays(HashSet<DataPoint> points)
     {
-        var tomorrow = DateOnly.FromDateTime(DateTime.Now).AddDays(1);
-        var firstDate = Dates.First();
+        var firstDate = Dates[0];
+        var daysInMonth = DateTime.DaysInMonth(firstDate.Year, firstDate.Month);
+
         // If the first day of the month is Thu, but the culture starts the week on Mon, the grid will have 3 preceding blank spaces, making the first index -2.
         var firstIndex = 1 - ((7 - ((_dateTimeFormat.FirstDayOfWeek - firstDate.DayOfWeek) % 7)) % 7);
-        var daysInMonth = DateTime.DaysInMonth(firstDate.Year, firstDate.Month);
 
         foreach (var gridIndex in Enumerable.Range(firstIndex, 7 * 6))
         {
             var day = gridIndex >= 1 && gridIndex <= daysInMonth ? gridIndex : (int?)null;
 
             // Bundle date during search to avoid additional DB lookups (thru DataPoint.Day).
-            var lookup = DataPoints.Select(p =>
-            {
-                var date = p.Day.Date;
-                return new DataPointLookup(p, date);
-            }).SingleOrDefault(l => l.Date.Month == firstDate.Month && l.Date.Day == gridIndex && l.Date <= tomorrow);
+            var lookup = points.Select(p => new DataPointLookup(p, p.Day.Date)).SingleOrDefault(l => l.Date.Day == gridIndex);
 
             // Make next search easier by removing uniquely dated point.
             if (lookup != null)
-                DataPoints.Remove(lookup.Point);
+            {
+                Debug.Assert(lookup.Date.Year == firstDate.Year && lookup.Date.Month == firstDate.Month, "Pool of data points is polluted from other months");
+                points.Remove(lookup.Point);
+            }
 
             var emoji = lookup?.Point?.Mood;
             var color = GetMoodColor(emoji);
@@ -79,24 +76,28 @@ public readonly struct GridMonth
 
 public readonly struct GridYear
 {
-    public GridYear(int year, HashSet<DataPoint> dataPoints)
+    public GridYear(int year, HashSet<DataPoint> allMoodPoints)
     {
         Year = year;
-        DataPoints = dataPoints;
-        GridMonths = GetGridMonths().ToList();
+        GridMonths = GetGridMonths(allMoodPoints).ToList();
     }
 
-    private IEnumerable<GridMonth> GetGridMonths()
+    private IEnumerable<GridMonth> GetGridMonths(IEnumerable<DataPoint> allMoodPoints)
     {
         var startDate = new DateOnly(Year, 1, 1);
         var endDate = new DateOnly(Year, 12, 31);
         var dates = startDate.DatesTo(endDate).ToList();
 
+        // Find all data points from the start of the year to at most tomorrow in system time.
+        var tomorrow = DateOnly.FromDateTime(DateTime.Now).Next();
+        var year = Year;
+        var yearPoints = allMoodPoints.Where(x => x.Day.Date.Year == year && x.Day.Date <= tomorrow).ToHashSet();
+
+        // Create months of the year.
         for (var i = 1; i <= 12; i++)
-            yield return new GridMonth(i, DataPoints, dates.Where(x => x.Month == i));
+            yield return new GridMonth(i, yearPoints.Where(x => x.Day.Date.Month == i), dates.Where(x => x.Month == i));
     }
 
     public int Year { get; }
-    public HashSet<DataPoint> DataPoints { get; }
     public IReadOnlyList<GridMonth> GridMonths { get; }
 }
