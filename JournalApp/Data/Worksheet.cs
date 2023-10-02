3120b5c54b5bae53768246a1e5704e7d4461a833
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-10-01 19:39:39 -0500

    Scaffolding for new Worksheets feature

---
+++ JournalApp/Data/Worksheet.cs
@@ -0,0 +1,14 @@
+﻿namespace JournalApp;
+
+public record class Worksheet
+{
+    public string Title { get; set; }
+
+    public string Uri { get; set; }
+
+    public string SourceUri { get; set; }
+
+    public string Category { get; set; }
+
+    public override string ToString() => $"{Title} @ {Uri}";
+}
---
+++ JournalApp/Data/Worksheets.cs
@@ -0,0 +1,35 @@
+﻿namespace JournalApp;
+
+public static class Worksheets
+{
+    private static IReadOnlyList<Worksheet> _collection;
+
+    public static IReadOnlyList<Worksheet> Collection => _collection ??= GetAllWorksheets().ToList();
+
+    private static IEnumerable<Worksheet> GetAllWorksheets()
+    {
+        yield return new()
+        {
+            Title = "My sample worksheet",
+            Uri = "https://www.iana.org/help/example-domains",
+            SourceUri = "https://example.com",
+            Category = "Samples",
+        };
+
+        yield return new()
+        {
+            Title = "My second sample worksheet",
+            Uri = "https://www.iana.org/help/example-domains",
+            SourceUri = "https://example.com",
+            Category = "Samples",
+        };
+
+        yield return new()
+        {
+            Title = "A sample worksheet in a different category",
+            Uri = "https://en.wikipedia.org/wiki/Domain_name",
+            SourceUri = "https://en.wikipedia.org",
+            Category = "Samples 2",
+        };
+    }
+}
