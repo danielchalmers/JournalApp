dde2d90032db46adfc140b0cafb34b362664008e
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-10-23 10:20:42 -0500

    Add Safety Plan preamble and more placeholders

--- JournalApp/SafetyPlanning/SafetyPlan.cs
+++ JournalApp/SafetyPlanning/SafetyPlan.cs
@@ -2,6 +2,7 @@
 
 public record SafetyPlan
 {
+    public static string Preamble { get; } = "A safety plan is a written list of coping strategies and sources of support can be used before or during a crisis. The plan should be brief, easy to read, and in your own words.";
     public string WarningSigns { get; set; }
     public string CopingStrategies { get; set; }
     public string SocialDistractions { get; set; }
