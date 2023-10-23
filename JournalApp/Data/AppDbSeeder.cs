4eaa3431776a4dc255902f752d579922403f6ec2
Daniel Chalmers <daniel.chalmers@outlook.com>
2023-10-23 17:00:50 -0500

    Update existing default categories with changed properties
    Otherwise we could never update them.

--- JournalApp/Data/AppDbSeeder.cs
+++ JournalApp/Data/AppDbSeeder.cs
@@ -54,117 +54,113 @@ public class AppDbSeeder
 
     private async Task SeedCategories()
     {
-        var existingCategoryGuids = db.Categories.Select(c => c.Guid).ToHashSet();
-
-        async Task TryAdd(DataPointCategory newCategory)
+        async Task AddOrUpdate(
+            string guid,
+            DataType dataType,
+            string group = null,
+            string name = null,
+            bool readOnly = true,
+            bool enabled = true,
+            decimal? medDose = null,
+            string medUnit = null,
+            DateTimeOffset? medEveryDaySince = null)
         {
-            if (!existingCategoryGuids.Contains(newCategory.Guid))
-                await db.AddCategory(newCategory);
+            // Apply the given values to a new category, or an existing one without replacing it.
+
+            var category = db.Categories.FirstOrDefault(x => x.Guid.ToString() == guid);
+            var isNew = category == null;
+            category ??= new();
+
+            category.Group = group;
+            category.Name = name;
+            category.ReadOnly = readOnly;
+            category.Enabled = enabled;
+            category.Type = dataType;
+            category.MedicationDose = medDose;
+            category.MedicationUnit = medUnit;
+            category.MedicationEveryDaySince = medEveryDaySince;
+
+            if (isNew)
+                await db.AddCategory(category);
         }
 
-        await TryAdd(new()
-        {
-            Guid = new("BF394F35-2228-4933-BF38-AF5B1B97AEF7"),
-            Group = "Notes",
-            Type = DataType.Note,
-            ReadOnly = true,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("D90D89FB-F5B9-47CF-AE4E-3EC0D635E783"),
-            Name = "Overall mood",
-            Type = DataType.Mood,
-            ReadOnly = true,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("D8657B36-F3A0-486F-BF80-0CF057919C7D"),
-            Name = "Last night's sleep",
-            Type = DataType.Sleep,
-            ReadOnly = true,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("7330B995-0B56-46FF-9DD6-9CFC550FF5C8"),
-            Name = "Most depressed mood",
-            Type = DataType.MildToSevere,
-            ReadOnly = true,
-            Enabled = false,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("4955EB49-0BCF-433B-873E-2092F292CC6B"),
-            Name = "Most elevated mood",
-            Type = DataType.MildToSevere,
-            ReadOnly = true,
-            Enabled = false,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("E9B7E4BE-FD17-4171-B1D4-D38B6009FDA0"),
-            Name = "Irritability",
-            Type = DataType.MildToSevere,
-            ReadOnly = true,
-            Enabled = false,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("0FB54AFF-9ECC-4C17-BAB5-B908B794CEA9"),
-            Name = "Anxiety",
-            Type = DataType.MildToSevere,
-            ReadOnly = true,
-            Enabled = false,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("40B5AF7B-4F4E-4E77-BD6B-F7855CF773AB"),
-            Name = "Productivity",
-            Type = DataType.LowToHigh,
-            ReadOnly = true,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("DE394B38-9007-4349-AE31-429541AAB947"),
-            Name = "Exercised or was active",
-            Type = DataType.Bool,
-            ReadOnly = true,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("EE8DE4D0-3A87-4CA4-B384-81BD7508A19F"),
-            Name = "Menstruating",
-            Type = DataType.Bool,
-            ReadOnly = true,
-            Enabled = false,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("C871C9F7-1A6E-4EA2-ACC9-94A256C9E2CC"),
-            Name = "Therapy",
-            Type = DataType.Bool,
-            ReadOnly = true,
-            Enabled = false,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("480DC07D-1330-486F-9B30-EC83A3D4E6F0"),
-            Name = "Weight",
-            Type = DataType.Number,
-            ReadOnly = true,
-        });
-        await TryAdd(new()
-        {
-            Guid = new("01A8F325-3002-40C4-B076-234E26172E82"),
-            Group = "Medications",
-            Name = "Vitamin D",
-            Type = DataType.Medication,
-            Index = 1,
-            MedicationDose = 2000,
-            MedicationUnit = "IU",
-            MedicationEveryDaySince = DateTimeOffset.Now,
-            ReadOnly = false,
-            Enabled = false,
-        });
+        await AddOrUpdate(
+            "BF394F35-2228-4933-BF38-AF5B1B97AEF7",
+            DataType.Note,
+            group: "Notes"
+        );
+        await AddOrUpdate(
+            "D90D89FB-F5B9-47CF-AE4E-3EC0D635E783",
+            DataType.Mood,
+            name: "Overall mood"
+        );
+        await AddOrUpdate(
+            "D8657B36-F3A0-486F-BF80-0CF057919C7D",
+            DataType.Sleep,
+            name: "Last night's sleep"
+        );
+        await AddOrUpdate(
+            "7330B995-0B56-46FF-9DD6-9CFC550FF5C8",
+            DataType.MildToSevere,
+            name: "Most depressed mood",
+            enabled: false
+        );
+        await AddOrUpdate(
+            "4955EB49-0BCF-433B-873E-2092F292CC6B",
+            DataType.MildToSevere,
+            name: "Most elevated mood",
+            enabled: false
+        );
+        await AddOrUpdate(
+            "E9B7E4BE-FD17-4171-B1D4-D38B6009FDA0",
+            DataType.MildToSevere,
+            name: "Irritability",
+            enabled: false
+        );
+        await AddOrUpdate(
+            "0FB54AFF-9ECC-4C17-BAB5-B908B794CEA9",
+            DataType.MildToSevere,
+            name: "Anxiety",
+            enabled: false
+        );
+        await AddOrUpdate(
+            "40B5AF7B-4F4E-4E77-BD6B-F7855CF773AB",
+            DataType.LowToHigh,
+            name: "Productivity"
+        );
+        await AddOrUpdate(
+            "DE394B38-9007-4349-AE31-429541AAB947",
+            DataType.Bool,
+            name: "Exercised or was active"
+        );
+        await AddOrUpdate(
+            "EE8DE4D0-3A87-4CA4-B384-81BD7508A19F",
+            DataType.Bool,
+            name: "Menstruating",
+            enabled: false
+        );
+        await AddOrUpdate(
+            "C871C9F7-1A6E-4EA2-ACC9-94A256C9E2CC",
+            DataType.Bool,
+            name: "Therapy",
+            enabled: false
+        );
+        await AddOrUpdate(
+            "480DC07D-1330-486F-9B30-EC83A3D4E6F0",
+            DataType.Number,
+            name: "Weight"
+        );
+        await AddOrUpdate(
+            "01A8F325-3002-40C4-B076-234E26172E82",
+            DataType.Medication,
+            group: "Medications",
+            name: "Vitamin D",
+            medDose: 2000,
+            medUnit: "IU",
+            medEveryDaySince: DateTimeOffset.Now,
+            readOnly: false,
+            enabled: false
+        );
     }
 
     private async Task SeedDays()
