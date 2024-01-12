43c44e13cb5b21fdc53712ed55ba2b7914a68859
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-01-12 15:43:20 -0600

    Refactor trends

---
+++ JournalApp/Components/Pages/Trends/PointOnChart.cs
@@ -0,0 +1,48 @@
+﻿namespace JournalApp;
+
+internal record PointOnChart(int Day, DataPoint Point)
+{
+    private static readonly List<string> ChartEmojis = ["😭", "😢", "😕", "😐", "🙂", "😀", "🤩"];
+
+    public decimal? Mood
+    {
+        get
+        {
+            var i = ChartEmojis.IndexOf(Point?.Mood);
+
+            return i == -1 ? null : i + 1;
+        }
+    }
+
+    public decimal? SleepHours => Point?.SleepHours;
+
+    public decimal? Scale => Point?.ScaleIndex;
+
+    public decimal? Number => (decimal?)Point?.Number;
+
+    public decimal? MedDose
+    {
+        get
+        {
+            if (Point?.Bool == true)
+                return Point.MedicationDose;
+            else if (Point?.Bool == false)
+                return 0;
+
+            return null;
+        }
+    }
+
+    public decimal? Bool
+    {
+        get
+        {
+            if (Point?.Bool == true)
+                return 1;
+            else if (Point?.Bool == false)
+                return 0;
+
+            return null;
+        }
+    }
+}
