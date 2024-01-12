9895cb37669c8fab228ced0dd5eeba67b03745eb
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-01-12 17:51:37 -0600

    Tweak feedback defaults
    Most importantly includes the version of the app now

--- JournalApp/Data/FeedbackService.cs
+++ JournalApp/Data/FeedbackService.cs
@@ -8,17 +8,12 @@ public static class FeedbackService
     public static string GenerateLink(string subject = null, string body = null)
     {
         var deviceInfo = DeviceInfo.Current;
+        var deviceInfoString = $"{ThisAssembly.AssemblyInformationalVersion} | {deviceInfo.Platform} {deviceInfo.VersionString} | {deviceInfo.Manufacturer} {deviceInfo.Model}";
 
-        var finalSubject = "JournalApp: ";
+        subject = "JournalApp: " + subject;
 
-        if (!string.IsNullOrWhiteSpace(subject))
-            finalSubject += subject;
+        body += "%0D%0A" + deviceInfoString + "%0D%0A";
 
-        var finalBody = $"{deviceInfo.Platform} {deviceInfo.VersionString}%0D%0A{deviceInfo.Manufacturer} {deviceInfo.Model}%0D%0A%0D%0A";
-
-        if (!string.IsNullOrWhiteSpace(body))
-            finalBody += body;
-
-        return $"mailto:{Email}?subject={finalSubject}&body={finalBody}".Replace(" ", "%20");
+        return $"mailto:{Email}?subject={subject}&body=%0D%0A{body}".Replace(" ", "%20");
     }
 }
