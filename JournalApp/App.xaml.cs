383faa1223e67e1cbf0d0d31ff96e085f6768dd4
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-01-20 23:20:09 -0600

    Follow up on 16181743150f194bfc7cd9012e118b79367b5af7

--- JournalApp/App.xaml.cs
+++ JournalApp/App.xaml.cs
@@ -8,4 +8,21 @@ public partial class App : Application
 
         MainPage = new MainPage();
     }
+
+    public static Window Window { get; private set; }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        Window = window;
+
+        Window.Destroying += App_Destroying;
+
+        return window;
+    }
+
+    private void App_Destroying(object sender, EventArgs e)
+    {
+        Preferences.Set("index_left_at", null);
+    }
 }
