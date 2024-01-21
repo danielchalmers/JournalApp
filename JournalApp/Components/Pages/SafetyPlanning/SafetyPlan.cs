803d693aa6de9ae6928ce2b36a0dcda6a3653d21
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-01-21 16:55:48 -0600

    Allow safety plan to be set up for before being listed in menu

--- JournalApp/Components/Pages/SafetyPlanning/SafetyPlan.cs
+++ JournalApp/Components/Pages/SafetyPlanning/SafetyPlan.cs
@@ -2,11 +2,11 @@
 
 public record SafetyPlan
 {
-    public string WarningSigns { get; set; }
-    public string CopingStrategies { get; set; }
-    public string SocialDistractions { get; set; }
-    public string SocialContacts { get; set; }
+    public string WarningSigns { get; set; } = "";
+    public string CopingStrategies { get; set; } = "";
+    public string SocialDistractions { get; set; } = "";
+    public string SocialContacts { get; set; } = "";
     public string ProfessionalContacts { get; set; } = "https://en.wikipedia.org/wiki/List_of_suicide_crisis_lines\n";
-    public string EnvironmentChanges { get; set; }
-    public string Purpose { get; set; }
+    public string EnvironmentChanges { get; set; } = "";
+    public string Purpose { get; set; } = "";
 }
