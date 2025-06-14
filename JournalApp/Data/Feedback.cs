a7fd525d02201df9b3addbfcca4f40571b95c73c
Daniel Chalmers <daniel.chalmers@outlook.com>
2025-06-14 00:01:45 -0500

    Remove email links

--- JournalApp/Data/Feedback.cs
+++ JournalApp/Data/Feedback.cs
@@ -2,18 +2,5 @@
 
 public static class Feedback
 {
-    public static readonly string Email = "DCAppHelp@outlook.com";
     public static readonly string NewGitHubIssue = "https://github.com/danielchalmers/JournalApp/issues/new";
-
-    public static string GenerateLink(string subject = null, string body = null)
-    {
-        var deviceInfo = DeviceInfo.Current;
-        var deviceInfoString = $"{ThisAssembly.AssemblyInformationalVersion} | {deviceInfo.Platform} {deviceInfo.VersionString} | {deviceInfo.Manufacturer} {deviceInfo.Model}";
-
-        subject = $"Good Diary: " + subject;
-
-        body += "%0D%0A" + deviceInfoString + "%0D%0A";
-
-        return $"mailto:{Email}?subject={subject}&body=%0D%0A{body}".Replace(" ", "%20");
-    }
 }
